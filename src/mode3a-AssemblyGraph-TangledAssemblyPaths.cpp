1638774f7e36f55b6f6dc04586ad4f3de1b7d859
Paolo Carnevali <pacarnev@ucsc.edu>
2023-02-07 10:53:33 -0800

    Fix missing line end in debug output.

--- src/mode3a-AssemblyGraph-TangledAssemblyPaths.cpp
+++ src/mode3a-AssemblyGraph-TangledAssemblyPaths.cpp
@@ -86,6 +86,7 @@ void AssemblyGraph::computeTangledAssemblyPath(
                 debugOut << vertexStringId(secondaryVertex.v) << " ";
             }
         }
+        debugOut << "\n";
     }
 
 }
