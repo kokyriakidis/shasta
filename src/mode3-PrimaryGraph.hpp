4a17c92c7da167f0ab542f95fb1dcbf6983d139d
Paolo Carnevali <pacarnev@ucsc.edu>
2024-04-01 10:34:45 -0700

    Remove class mode3::GlobalPathGraph, as it is no longer used.

--- src/mode3-PrimaryGraph.hpp
+++ src/mode3-PrimaryGraph.hpp
@@ -29,14 +29,10 @@ namespace shasta {
     class MarkerGraph;
     namespace mode3 {
 
-        // The global path graph.
-        // Each vertex corresponds to a primary marker graph edge.
-        class GlobalPathGraph;
-        class GlobalPathGraphVertex;
-        class GlobalPathGraphEdge;
-
-        // A subset of the GlobalPathGraph, for example
-        // a single connected component, represented as a Boost graph.
+        // A connected component of the primary graph,
+        // in which each vertex represents a primary edge of the marker graph.
+        // Edges are created by following the reads on their journeys
+        // over primary marker graph edges.
         class PrimaryGraphVertex;
         class PrimaryGraphEdge;
         class PrimaryGraph;
@@ -54,7 +50,7 @@ namespace shasta {
 
 
 
-// Class to control Graphviz output of GlobalPathGraph and PathGraph.
+// Class to control Graphviz output of PrimaryGraph.
 class shasta::mode3::GlobalPathGraphDisplayOptions {
 public:
     bool labels = true;
@@ -149,118 +145,4 @@ public:
     void removeWeakEdges(double maxLoss);
 
 };
-
-
-
-class shasta::mode3::GlobalPathGraphVertex {
-public:
-    MarkerGraphEdgeId edgeId;
-
-    GlobalPathGraphVertex(MarkerGraphEdgeId edgeId) : edgeId(edgeId) {}
-
-    // Compare by edgeId only.
-    bool operator<(const GlobalPathGraphVertex& that) const
-    {
-        return edgeId < that.edgeId;
-    }
-};
-
-
-
-
-class shasta::mode3::GlobalPathGraphEdge {
-public:
-    uint64_t vertexId0;
-    uint64_t vertexId1;
-    MarkerGraphEdgePairInfo info;
-
-    // The number of oriented reads for which the journey contains
-    // a transition from vertexId0 to vertexId1.
-    // This is less than or equal to info.common.
-    uint64_t coverage = invalid<uint64_t>;
-};
-
-
-
-class shasta::mode3::GlobalPathGraph {
-public:
-    static void assemble(
-        const Assembler&,
-        uint64_t threadCount0,
-        uint64_t threadCount1);
-    static void loadAndAssemble(
-        const Assembler&,
-        const string& fileName,
-        uint64_t threadCount0,
-        uint64_t threadCount1);
-private:
-    GlobalPathGraph(const Assembler&);
-    const Assembler& assembler;
-
-    // Each vertex corresponds to a primary marker graph edge.
-    // Store them here.
-    // The index in this table is the vertexId.
-    // The table is sorted by MarkerGraphEdgeId.
-    // It cannot be named "vertices" because of name conflicts with the Boost graph
-    // iteration macros.
-    vector<GlobalPathGraphVertex> verticesVector;
-    void createVertices();
-
-    // Return the vertexId corresponding to a given MarkerGraphEdgeId, or
-    // invalid<MarkerGraphEdgeId> if no such a vertex exists.
-    // This uses a binary search in the verticesVector (via std::lower_bound).
-    uint64_t getVertexId(MarkerGraphEdgeId) const;
-
-    vector<GlobalPathGraphEdge> edges;
-    void createEdges();
-
-    // The connected components of the GlobalPathGraph.
-    // Stored sorted by decreasing size, as measured by number of vertices.
-    vector< shared_ptr<PrimaryGraph> > components;
-
-    // Create connected components.
-    // This only considers edges with corrected Jaccard at least equal to
-    // minCorrectedJaccard, and only stores connected components with at
-    // least minComponentSize vertices.
-    void createComponents(
-        double minCorrectedJaccard,
-        uint64_t minComponentSize);
-
-    // Local transitive reduction of each connected component.
-    // Only edges with coverage up to maxCoverage are subject to flagging
-    // as removed during transitive reduction.
-    void localTransitiveReduction(
-        uint64_t distance,
-        uint64_t maxCoverage);
-
-    // Write the entire PathGraph in graphviz format.
-    void writeGraphviz() const;
-
-    // Write each connected component in graphviz format.
-    void writeComponentsGraphviz(
-        const string& baseName,
-        const GlobalPathGraphDisplayOptions&) const;
-
-    void assembleComponent(
-        uint64_t componentId,
-        double maxLoss,
-        uint64_t transitiveReductionDistance,
-        uint64_t transitiveReductionMaxCoverage,
-        uint64_t crossEdgesLowCoverageThreshold,
-        uint64_t crossEdgesHighCoverageThreshold,
-        uint64_t crossEdgesMinOffset,
-        uint64_t threadCount0,
-        uint64_t threadCount1);
-
-public:
-    static void searchForDetangling(
-        const array<MarkerGraphEdgeId, 2>& in,
-        const array<MarkerGraphEdgeId, 2>& out,
-        uint64_t highCommonCountThreshold,
-        uint64_t lowCommonCountThreshold,
-        const Assembler&,
-        array<array<vector<MarkerGraphEdgeId>, 2>, 2>&);
-
-};
-
 
