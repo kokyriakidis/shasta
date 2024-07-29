46f5c920dd79144af185b4352c130ff34a7c1880
Paolo Carnevali <pacarnev@ucsc.edu>
2024-07-29 10:55:01 -0700

    Move Assembler::createPrimaryMarkerGraphEdges to AssemblerMode3.cpp.

--- src/AssemblerMode3.cpp
+++ src/AssemblerMode3.cpp
@@ -5,7 +5,9 @@
 #include "mode3-AssemblyGraph.hpp"
 #include "mode3-AnchorGraph.hpp"
 #include "Mode3Assembler.hpp"
+#include "performanceLog.hpp"
 #include "Reads.hpp"
+#include "timestamp.hpp"
 using namespace shasta;
 
 // Boost libraries.
@@ -282,4 +284,246 @@ void Assembler::fillMode3AssemblyPathStep(const vector<string>& request, ostream
         options,
         httpServerData.assemblerOptions->assemblyOptions.mode3Options.localAssemblyOptions,
         useA, useB);
+}
+
+
+
+// Creation of marker graph edges (anchors) for Mode 3 assembly.
+// - Like createMarkerGraphEdgesStrict, this
+//   will only create edges in which all contributing oriented reads have
+//   exactly the same sequence. If more than one distinct sequence
+//   is present, the edge is split into two parallel edges.
+// - This only generates primary marker graph edges, defined as follows:
+//   * Edge coverage is >= minPrimaryCoverage and <= maxPrimaryCoverage.
+//   * Both its vertices have no duplicate ReadId.
+//   * The edge marker interval has no duplicate ReadId.
+// - This will only create the MarkerGraph::edgeMarkerIntervals
+//   and nothing else.
+void Assembler::createPrimaryMarkerGraphEdges(
+    uint64_t minPrimaryCoverage,
+    uint64_t maxPrimaryCoverage,
+    uint64_t threadCount)
+{
+    performanceLog << timestamp << "createPrimaryMarkerGraphEdges begins." << endl;
+
+    // Check that we have what we need.
+    checkMarkersAreOpen();
+    checkMarkerGraphVerticesAreAvailable();
+    SHASTA_ASSERT(markerGraph.disjointSetsHistogram.isOpen);
+
+    // Adjust the numbers of threads, if necessary.
+    if(threadCount == 0) {
+        threadCount = std::thread::hardware_concurrency();
+    }
+
+    // If the coverage range for primary marker graph edges is not
+    // specified, use the disjoint sets histogram to compute reasonable values.
+    if((minPrimaryCoverage == 0) and (maxPrimaryCoverage == 0)) {
+        tie(minPrimaryCoverage, maxPrimaryCoverage) = getPrimaryCoverageRange();
+    }
+
+    // Store the arguments so the threads can see them.
+    createPrimaryMarkerGraphEdgesData.minPrimaryCoverage = minPrimaryCoverage;
+    createPrimaryMarkerGraphEdgesData.maxPrimaryCoverage = maxPrimaryCoverage;
+
+    // Make space for the edges found by each thread.
+    createPrimaryMarkerGraphEdgesData.threadMarkerIntervals.resize(threadCount);
+    createPrimaryMarkerGraphEdgesData.threadSequences.resize(threadCount);
+
+    // Parallelize over the source vertex of primary marker graph edges.
+    // Each thread stores in a separate vector the marker intervals for the primary edges it finds.
+    uint64_t batchSize = 1000;
+    const MemoryMapped::VectorOfVectors<MarkerId, MarkerGraph::CompressedVertexId>& markerGraphVertices =
+        *markerGraph.verticesPointer;
+    setupLoadBalancing(markerGraphVertices.size(), batchSize);
+    runThreads(&Assembler::createPrimaryMarkerGraphEdgesThreadFunction, threadCount);
+
+    // Gather the primary edges found by all threads.
+    markerGraph.edgeMarkerIntervals.createNew(
+            largeDataName("GlobalMarkerGraphEdgeMarkerIntervals"),
+            largeDataPageSize);
+    markerGraph.edgeSequence.createNew(
+        largeDataName("MarkerGraphEdgesSequence"), largeDataPageSize);
+    for(uint64_t threadId=0; threadId<threadCount; threadId++) {
+        auto& threadMarkerIntervals = *createPrimaryMarkerGraphEdgesData.threadMarkerIntervals[threadId];
+        auto& threadSequences = *createPrimaryMarkerGraphEdgesData.threadSequences[threadId];
+        SHASTA_ASSERT(threadMarkerIntervals.size() == threadSequences.size());
+        for(uint64_t i=0; i<threadMarkerIntervals.size(); i++) {
+            const span<MarkerInterval> markerIntervals = threadMarkerIntervals[i];
+            markerGraph.edgeMarkerIntervals.appendVector(markerIntervals.begin(), markerIntervals.end());
+            const span<Base> sequence = threadSequences[i];
+            markerGraph.edgeSequence.appendVector(sequence.begin(), sequence.end());
+        }
+        threadMarkerIntervals.remove();
+        threadSequences.remove();
+        createPrimaryMarkerGraphEdgesData.threadMarkerIntervals[threadId] = 0;
+        createPrimaryMarkerGraphEdgesData.threadSequences[threadId] = 0;
+    }
+    createPrimaryMarkerGraphEdgesData.threadMarkerIntervals.clear();
+    createPrimaryMarkerGraphEdgesData.threadSequences.clear();
+    cout << "Found " << markerGraph.edgeMarkerIntervals.size() << " primary marker graph edges." << endl;
+
+    performanceLog << timestamp << "createPrimaryMarkerGraphEdges ends." << endl;
+}
+
+
+
+void Assembler::createPrimaryMarkerGraphEdgesThreadFunction(uint64_t threadId)
+{
+    // Sanity check and get k/2.
+    const uint64_t k = assemblerInfo->k;
+    SHASTA_ASSERT((k % 2) == 0);
+    const uint32_t kHalf = uint32_t(k / 2);
+    SHASTA_ASSERT(getReads().representation == 0);
+
+    // Access the data set up by createPrimaryMarkerGraphEdges.
+    auto& data = createPrimaryMarkerGraphEdgesData;
+
+    // Get the primary coverage range.
+    const uint64_t minPrimaryCoverage = data.minPrimaryCoverage;
+    const uint64_t maxPrimaryCoverage = data.maxPrimaryCoverage;
+
+    // Create the vector to contain the marker intervals for the edges found by this thread.
+    shared_ptr< MemoryMapped::VectorOfVectors<MarkerInterval, uint64_t> > markerIntervalsPointer =
+        make_shared< MemoryMapped::VectorOfVectors<MarkerInterval, uint64_t> >();
+    data.threadMarkerIntervals[threadId] = markerIntervalsPointer;
+    MemoryMapped::VectorOfVectors<MarkerInterval, uint64_t>&
+        markerIntervals = *markerIntervalsPointer;
+    markerIntervals.createNew(
+            largeDataName("tmp-ThreadMarkerGraphEdgeMarkerIntervals-" + to_string(threadId)),
+            largeDataPageSize);
+
+    // Create the vector to contain the sequences for the edges found by this thread.
+    shared_ptr< MemoryMapped::VectorOfVectors<Base, uint64_t> > sequencesPointer =
+        make_shared< MemoryMapped::VectorOfVectors<Base, uint64_t> >();
+    data.threadSequences[threadId] = sequencesPointer;
+    MemoryMapped::VectorOfVectors<Base, uint64_t>&
+        sequences = *sequencesPointer;
+    sequences.createNew(
+            largeDataName("tmp-ThreadMarkerGraphSequences-" + to_string(threadId)),
+            largeDataPageSize);
+
+
+    // Access data structures we need.
+    const Reads& reads = getReads();
+    const MemoryMapped::VectorOfVectors<MarkerId, MarkerGraph::CompressedVertexId>& markerGraphVertices =
+        markerGraph.vertices();
+
+    // For each sequence and next vertex we encounter, we store the corresponding MarkerIntervals.
+    class Info {
+    public:
+        vector<Base> sequence;
+        MarkerGraphVertexId vertexId1;
+        vector<MarkerInterval> markerIntervals;
+        Info(
+            const vector<Base>& sequence,
+            MarkerGraphVertexId vertexId1,
+            const MarkerInterval& markerInterval) :
+            sequence(sequence),
+            vertexId1(vertexId1),
+            markerIntervals(1, markerInterval) {}
+    };
+    vector<Info> infos;
+
+    // Work vector used in the loop below.
+    vector<Base> sequence;
+
+    // Loop over all batches assigned to this thread.
+    uint64_t begin, end;
+    while(getNextBatch(begin, end)) {
+
+        // Loop over source vertex ids assigned to this batch.
+        for(MarkerGraphVertexId vertexId0=begin; vertexId0!=end; vertexId0++) {
+
+            infos.clear();
+
+            // Access the markers of this vertex.
+            const auto vertexMarkerIds = markerGraphVertices[vertexId0];
+
+            // If vertex coverage of this vertex is less than minPrimaryCoverage,
+            // no primary edges start here for sure.
+            if(vertexMarkerIds.size() < minPrimaryCoverage) {
+                continue;
+            }
+
+            // If this vertex has duplicate ReadIds, no primary edge can begin here.
+            if(markerGraph.vertexHasDuplicateReadIds(vertexId0, markers)) {
+                continue;
+            }
+
+            // Loop over the markers of this vertex.
+            for(const MarkerId markerId0: vertexMarkerIds) {
+                OrientedReadId orientedReadId;
+                uint32_t ordinal0;
+                tie(orientedReadId, ordinal0) = findMarkerId(markerId0);
+
+                // Find the next marker graph vertex visited by this OrientedReadId.
+                const uint32_t ordinal1 = ordinal0 + 1;
+                const auto orientedReadMarkers = markers[orientedReadId.getValue()];
+                if(ordinal1 == orientedReadMarkers.size()) {
+                    // There is no next vertex.
+                    continue;
+                }
+                SHASTA_ASSERT(ordinal1 < orientedReadMarkers.size());
+                const MarkerId markerId1 = markerId0 + 1;
+                const MarkerGraphVertexId vertexId1 = markerGraph.vertexTable[markerId1];
+
+                // If vertexId1 has coverage less than minPrimaryCoverage, there
+                // cannot be a primary edge vertexId0->vertexId1.
+                if(markerGraphVertices.size(vertexId1) < minPrimaryCoverage) {
+                    continue;
+                }
+
+                // Get the sequence between these two markers.
+                const uint32_t position0 = uint32_t(markers.begin()[markerId0].position) + kHalf;
+                const uint32_t position1 = uint32_t(markers.begin()[markerId1].position) + kHalf;
+                sequence.clear();
+                for(uint32_t position=position0; position<position1; position++) {
+                    const Base base = reads.getOrientedReadBase(orientedReadId, position);
+                    sequence.push_back(base);
+                }
+
+                // Construct this MarkerInterval.
+                MarkerInterval markerInterval;
+                markerInterval.orientedReadId = orientedReadId;
+                markerInterval.ordinals[0] = ordinal0;
+                markerInterval.ordinals[1] = ordinal1;
+
+                // If we already have an Info with this sequence and vertexId1, add id there.
+                // Otherwise create a new Info.
+                bool found = false;
+                for(Info& info: infos) {
+                    if(info.sequence == sequence and info.vertexId1 == vertexId1) {
+                        info.markerIntervals.push_back(markerInterval);
+                        found = true;
+                        break;
+                    }
+                }
+                if(not found) {
+                    infos.push_back(Info(sequence, vertexId1, markerInterval));
+                }
+            }
+
+            // Each of our Infos object can generate a primary edge, but we have to check that:
+            // - Coverage is in the primary coverage range.
+            // - vertexId1 does not have duplicate ReadIds.
+            for(const Info& info: infos) {
+                if(info.markerIntervals.size() < minPrimaryCoverage) {
+                    continue;
+                }
+                if(info.markerIntervals.size() > maxPrimaryCoverage) {
+                    continue;
+                }
+                if(markerGraph.vertexHasDuplicateReadIds(info.vertexId1, markers)) {
+                    continue;
+                }
+
+                // Generate a new primary marker graph edge.
+                markerIntervals.appendVector(info.markerIntervals);
+                sequences.appendVector(info.sequence);
+            }
+
+        }
+    }
+
 }
