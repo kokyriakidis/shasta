569d1be0c83bdf9750f07220c74a643fed8fa2af
Paolo Carnevali <pacarnev@ucsc.edu>
2025-01-13 10:58:53 -0800

    Move detangle2 code to mode3::Detangle2Graph.

--- src/mode3-AssemblyGraphDetangle2.cpp
+++ src/mode3-AssemblyGraphDetangle2.cpp
@@ -1,5 +1,6 @@
 // Shasta.
 #include "mode3-AssemblyGraph.hpp"
+#include "mode3-Detangle2.hpp"
 using namespace shasta;
 using namespace mode3;
 
@@ -119,11 +120,13 @@ void AssemblyGraph::detangle2()
 
     dot << "}\n";
 }
-#else
+#endif
 
 
 
 // Detangling with path following.
+// When this is called, all the BubbleChains must consist of a single Chain.
+// This can be achieved by calling expand first.
 void AssemblyGraph::detangle2()
 {
     AssemblyGraph& assemblyGraph = *this;
@@ -138,7 +141,6 @@ void AssemblyGraph::detangle2()
     cout << num_vertices(assemblyGraph) << " vertices." << endl;
     cout << num_edges(assemblyGraph) << " edges." << endl;
 
-    // Check that all the BubbleChains consist of a single Chain.
     BGL_FORALL_EDGES(e, assemblyGraph, AssemblyGraph) {
         SHASTA_ASSERT(assemblyGraph[e].isSimpleChain());
     }
@@ -146,6 +148,7 @@ void AssemblyGraph::detangle2()
     // Make sure we have anchor annotations.
     annotateAnchors();
 
+#if 0
     // Gather the long chains.
     std::set<edge_descriptor> longChains;
     BGL_FORALL_EDGES(e, assemblyGraph, AssemblyGraph) {
@@ -154,8 +157,15 @@ void AssemblyGraph::detangle2()
             longChains.insert(e);
         }
     }
+#endif
+
+    // Create the Detangle2Graph.
+    Detangle2Graph detangle2Graph(assemblyGraph, chainLengthThreshold);
+    detangle2Graph.addEdges();
+    detangle2Graph.writeGraphviz();
 
 
+#if 0
     // Main loop over the long chains.
     for(const edge_descriptor e0: longChains) {
 
@@ -171,10 +181,11 @@ void AssemblyGraph::detangle2()
         edge_descriptor eNew;
         detangle2Chain(e0, e1, eNew, longChains);
     }
+#endif
 }
 
 
-
+#if 0
 // This attempts to detangle the e0 Chain by finding a forward path
 // that connects it to another Chain e1.
 // If successful, it stitches e0 and e1 together to create
@@ -345,3 +356,196 @@ void AssemblyGraph::detangle2PathFollowing(
 }
 
 #endif
+
+
+
+// Construct the vertices from the long Chains of the AssemblyGraph.
+Detangle2Graph::Detangle2Graph(
+    const AssemblyGraph& assemblyGraph,
+    uint64_t chainLengthThreshold) :
+    assemblyGraph(assemblyGraph)
+{
+    Detangle2Graph& detangle2Graph = *this;
+
+    // Loop over all AssemblyGraph edges.
+    BGL_FORALL_EDGES(e, assemblyGraph, AssemblyGraph) {
+
+        // Get the BubbleChain corresponding to thsi AssemblyGraph edge.
+        const BubbleChain& bubbleChain = assemblyGraph[e];
+
+        // It must consist of a single Chain.
+        SHASTA_ASSERT(bubbleChain.isSimpleChain());
+        const Chain& chain = assemblyGraph[e].getOnlyChain();
+
+        // If long enough, generate a vertex.
+        const uint64_t offset = assemblyGraph.chainOffset(chain);
+        if(offset >= chainLengthThreshold) {
+            const vertex_descriptor v = add_vertex(Detangle2GraphVertex(e, offset), detangle2Graph);
+            vertexMap.insert(make_pair(e, v));
+        }
+    }
+}
+
+
+
+void Detangle2Graph::addEdges()
+{
+    Detangle2Graph& detangle2Graph = *this;
+
+    BGL_FORALL_VERTICES(v, detangle2Graph, Detangle2Graph) {
+        cout << "Working on " << assemblyGraph.bubbleChainStringId(detangle2Graph[v].e) << endl;
+        findForwardPath(v);
+        findBackwardPath(v);
+    }
+}
+
+
+
+void Detangle2Graph::findForwardPath(vertex_descriptor v0)
+{
+    findPath(v0, 0);
+}
+
+
+
+void Detangle2Graph::findBackwardPath(vertex_descriptor v0)
+{
+    findPath(v0, 1);
+}
+
+
+
+// Find a path starting at v0 and create edges if appropriate.
+// Direction is 0 for a forward path and 1 for a backward path.
+void Detangle2Graph::findPath(vertex_descriptor v0, uint64_t direction)
+{
+    Detangle2Graph& detangle2Graph = *this;
+
+    const AssemblyGraph::edge_descriptor e0 = detangle2Graph[v0].e;
+    const Chain& chain = assemblyGraph[e0].getOnlyChain();
+
+    std::set<edge_descriptor> longChainsFound;
+
+    // EXPOSE WHEN CODE STABILIZES.
+    const uint64_t seedAnchorCount = 10;
+    uint64_t minCommonCount = 4;
+    double minJaccard = 0;
+    double minCorrectedJaccard = 0.8;
+
+    // We do a recursive search starting from the last/first seedAnchorCount internal anchors of e0.
+    std::vector<AnchorInfo> h;
+    std::set<AnchorId> anchorIdsEncountered;
+    if(direction == 0) {
+        for(uint64_t i=0; i<seedAnchorCount; i++) {
+            const uint64_t positionInChain = chain.size() - 2 - i;
+            if(positionInChain == 0) {
+                break;
+            }
+            const AnchorId anchorId = chain[positionInChain];
+            h.push_back(AnchorInfo(anchorId, 0));
+            std::push_heap(h.begin(), h.end());
+            anchorIdsEncountered.insert(anchorId);
+        }
+    } else {
+        for(uint64_t i=0; i<seedAnchorCount; i++) {
+            const uint64_t positionInChain = i + 1;
+            if(positionInChain == chain.size() - 1) {
+                break;
+            }
+            const AnchorId anchorId = chain[positionInChain];
+            h.push_back(AnchorInfo(anchorId, 0));
+            std::push_heap(h.begin(), h.end());
+            anchorIdsEncountered.insert(anchorId);
+        }
+    }
+
+
+
+    // Main recursive loop.
+    vector< pair<AnchorId, AnchorPairInfo> > anchorIds1;
+    while(not h.empty()) {
+        std::pop_heap(h.begin(), h.end());
+        const AnchorInfo& anchorInfo0 = h.back();
+        const AnchorId anchorId0 = anchorInfo0.anchorId;
+        const uint64_t offset0 = anchorInfo0.offset;
+        h.pop_back();
+
+        // Path following starting at anchorId0;
+        assemblyGraph.anchors.followOrientedReads(anchorId0, direction,
+            minCommonCount, minJaccard, minCorrectedJaccard, anchorIds1);
+
+        bool foundLongChain = false;
+        for(const auto& p: anchorIds1) {
+            const AnchorId anchorId1 = p.first;
+            const AnchorPairInfo& info1 = p.second;
+            if(not anchorIdsEncountered.contains(anchorId1)) {
+                anchorIdsEncountered.insert(anchorId1);
+
+                // Check the annotation for this anchor.
+                const uint64_t localAnchorId1 = assemblyGraph.anchors.getLocalAnchorIdInComponent(anchorId1);
+                const auto& internalChainInfo = assemblyGraph.anchorAnnotations[localAnchorId1].internalChainInfo;
+                if(internalChainInfo.size() == 1)  {
+                    const pair<ChainIdentifier, uint64_t>& p = internalChainInfo.front();
+                    const ChainIdentifier& chainIdentifier = p.first;
+                    SHASTA_ASSERT(chainIdentifier.positionInBubbleChain == 0);
+                    SHASTA_ASSERT(chainIdentifier.indexInBubble == 0);
+                    const edge_descriptor e1 = chainIdentifier.e;
+                    if((e1 != e0) and vertexMap.contains(e1)) {
+                        longChainsFound.insert(e1);
+                        if(longChainsFound.size() >= 1) {
+                            const auto it1 = vertexMap.find(e1);
+                            SHASTA_ASSERT(it1 != vertexMap.end());
+                            const vertex_descriptor v1 = it1->second;
+                            if(direction == 0) {
+                                add_edge(v0, v1, detangle2Graph);
+                            } else {
+                                add_edge(v1, v0, detangle2Graph);
+                            }
+                            foundLongChain = true;
+                            break;
+                        }
+                    }
+                }
+
+                h.push_back(AnchorInfo(anchorId1, offset0 + info1.offsetInBases));
+                std::push_heap(h.begin(), h.end());
+
+            }
+            if(foundLongChain) {
+                break;
+            }
+        }
+        if(foundLongChain) {
+            break;
+        }
+    }
+
+
+}
+
+
+
+void Detangle2Graph::writeGraphviz() const
+{
+    const Detangle2Graph& detangle2Graph = *this;
+
+    ofstream dot("Detangle2.dot");
+    dot << "digraph Detangle2 {\n";
+
+    BGL_FORALL_VERTICES(v, detangle2Graph, Detangle2Graph) {
+        const AssemblyGraph::edge_descriptor e = detangle2Graph[v].e;
+        dot << "\"" << assemblyGraph.bubbleChainStringId(e) << "\";\n";
+    }
+
+    BGL_FORALL_EDGES(e, detangle2Graph, Detangle2Graph) {
+        const vertex_descriptor v0 = source(e, detangle2Graph);
+        const vertex_descriptor v1 = target(e, detangle2Graph);
+        const AssemblyGraph::edge_descriptor ae0 = detangle2Graph[v0].e;
+        const AssemblyGraph::edge_descriptor ae1 = detangle2Graph[v1].e;
+        dot << "\"" << assemblyGraph.bubbleChainStringId(ae0) << "\""
+            "->\"" << assemblyGraph.bubbleChainStringId(ae1) << "\";\n";
+    }
+
+    dot << "}\n";
+
+}
---
+++ src/mode3-Detangle2.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+// Class mode3::Detangle2 and related classes used by mode3::AssemblyGraph::detangle2.
+
+// Shasta.
+#include "mode3-AssemblyGraph.hpp"
+
+// Boost libraries.
+#include <boost/graph/adjacency_list.hpp>
+
+// Standard library.
+#include "array.hpp"
+
+namespace shasta {
+    namespace mode3 {
+
+        class Detangle2Graph;
+        class Detangle2GraphVertex;
+        class Detangle2GraphEdge;
+
+        using Detangle2GraphBaseClass = boost::adjacency_list<
+            boost::listS,
+            boost::listS,
+            boost::bidirectionalS,
+            Detangle2GraphVertex,
+            Detangle2GraphEdge>;
+
+    }
+}
+
+
+
+// A Detangle2GraphVertex corresponds to an AssemblyGraph which,
+// by construction, is a BubbleChain consisting of a single, long Chain.
+class shasta::mode3::Detangle2GraphVertex {
+public:
+    AssemblyGraph::edge_descriptor e;
+    uint64_t offset;
+
+    Detangle2GraphVertex(
+        AssemblyGraph::edge_descriptor e,
+        uint64_t offset) :
+        e(e), offset(offset) {}
+};
+
+
+
+// A Detangle2GraphEdge v0->v1  (Chain0->Chain1) is created if we found:
+// - A forward path starting at Chain0 and ending at Chain1.
+// and/or
+// - A backward path starting at Chain1 and ending at Chain0.
+class shasta::mode3::Detangle2GraphEdge {
+public:
+
+    // Flags to indicate if we found the forward path and/or the backward path.
+    array<bool, 2> found = {false, false};
+};
+
+
+
+class shasta::mode3::Detangle2Graph : public Detangle2GraphBaseClass {
+public:
+
+    // Construct the vertices from the long Chains of the AssemblyGraph.
+    Detangle2Graph(
+        const AssemblyGraph&,
+        uint64_t chainLengthThreshold);
+    void addEdges();
+    void writeGraphviz() const;
+
+private:
+    const AssemblyGraph& assemblyGraph;
+    std::map<AssemblyGraph::edge_descriptor, vertex_descriptor> vertexMap;
+
+    // Find forward and backward paths starting at the Chain corresponding
+    // to a given AssemblyGraph edge, and generate edges when successful.
+    void findForwardPath(vertex_descriptor);
+    void findBackwardPath(vertex_descriptor);
+    void findPath(vertex_descriptor, uint64_t direction);
+
+    class AnchorInfo {
+    public:
+        AnchorId anchorId;
+        uint64_t offset;
+        AnchorInfo(AnchorId anchorId, uint64_t offset) : anchorId(anchorId), offset(offset) {}
+        bool operator<(const AnchorInfo& that) const {
+            return offset > that.offset;
+        }
+    };
+};
+
