1ec8c39d58487fcf99e41b0b6a480b9bcf38718e
Paolo Carnevali <pacarnev@ucsc.edu>
2025-03-03 09:23:02 -0800

    Add -DSHASTA_LONG_MARKERS to enable longer markers (not yet functional).

--- src/shastaTypes.hpp
+++ src/shastaTypes.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#ifdef SHASTA_LONG_MARKERS
 #include <boost/multiprecision/cpp_int.hpp>
+#endif
 
 #include <cstdint.hpp>
 
@@ -11,8 +13,13 @@ namespace shasta {
     using KmerId16 = uint32_t;
     using KmerId32 = uint64_t;
     using KmerId64 = __uint128_t;
+
+#ifdef SHASTA_LONG_MARKERS
     using KmerId128 = boost::multiprecision::uint256_t;
+    using KmerId = KmerId128;
+#else
     using KmerId = KmerId64;
+#endif
 
     using ReadId = uint32_t;
     using Strand = ReadId;
