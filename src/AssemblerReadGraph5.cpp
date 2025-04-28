0dbf8f011158db4a4bd4e37ae89e7f245b6ad2b8
Paolo Carnevali <pacarnev@ucsc.edu>
2025-04-28 08:28:22 -0700

    Add boilerplate code for read graph creation method 5.

---
+++ src/AssemblerReadGraph5.cpp
@@ -0,0 +1,16 @@
+#include "Assembler.hpp"
+using namespace shasta;
+
+
+
+void Assembler::createReadGraph5()
+{
+    SHASTA_ASSERT(0);
+}
+
+
+
+void Assembler::createReadGraph5ThreadFunction(uint64_t /* threadId */)
+{
+    SHASTA_ASSERT(0);
+}
